ab22329895f4cc70b20be3595f9c62823439f086
Abdimalik-Osman <engabdimalik8818@gmail.com>
2024-03-30 12:09:01 +0300

    git checkout -b your-branch-name

--- frontend/src/pages/DeletedNote.jsx
+++ frontend/src/pages/DeletedNote.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import SideBar from '../components/SideBar'
 import {useParams,useNavigate} from 'react-router-dom'
 import axios from 'axios'
